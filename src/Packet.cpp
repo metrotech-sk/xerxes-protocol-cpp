840fe51bdbc10b7165225f6dd2d2075c7763f05d
theMladyPan <stanislav@rubint.sk>
2024-04-21 15:38:02 +0200

    Refactor Packet class constructor

--- src/Packet.cpp
+++ src/Packet.cpp
@@ -1,148 +1,136 @@
 #include "Packet.hpp"
 #include <sstream>
 
-
 namespace Xerxes
 {
 
-
-Packet::Packet()
-{
-}
-
-Packet::Packet(std::vector<uint8_t> &message)
-{
-    checksum = 0;
-    uint8_t msgLen = message.size() + 3;
-    _data.emplace_back(SOH); // start of packet
-    _data.emplace_back(msgLen); // message length
-    checksum = SOH + msgLen;
-
-    for(const auto &el:message)
+    Packet::Packet()
     {
-        _data.emplace_back(el);
-        checksum += el;
     }
 
-    checksum = ~(checksum & 0xFF) + 1; // two's complement
-    _data.emplace_back(checksum);
-}
-
-
-Packet::Packet(const std::vector<uint8_t> &message)
-{
-    checksum = 0;
-    uint8_t msgLen = message.size() + 3;
-    _data.emplace_back(SOH); // start of packet
-    _data.emplace_back(msgLen); // message length
-    checksum = SOH + msgLen;
-
-    for(const auto &el:message)
+    Packet::Packet(std::vector<uint8_t> &message)
     {
-        _data.emplace_back(el);
-        checksum += el;
+        checksum = 0;
+        uint8_t msgLen = message.size() + 3;
+        _data.emplace_back(SOH);    // start of packet
+        _data.emplace_back(msgLen); // message length
+        checksum = SOH + msgLen;
+
+        for (const auto &el : message)
+        {
+            _data.emplace_back(el);
+            checksum += el;
+        }
+
+        checksum = ~(checksum & 0xFF) + 1; // two's complement
+        _data.emplace_back(checksum);
     }
 
-    checksum = ~(checksum & 0xFF) + 1; // two's complement
-    _data.emplace_back(checksum);
-}
-
-
-Packet::~Packet()
-{
-}
-
-
-const uint8_t* Packet::data() const
-{
-    return _data.data();
-}
-
-
-size_t Packet::size() const
-{
-    return _data.size();
-}
-
-
-std::vector<uint8_t> Packet::getData() const
-{
-    return this->_data;
-}
-
-
-Packet Packet::EmptyPacket()
-{
-    std::vector<uint8_t> emptyVector {};
-    return Packet(emptyVector);
-}
-
-
-uint8_t Packet::at(const uint8_t pos) const
-{
-    return _data.at(pos);
-}
-
-
-void Packet::setData(const std::vector<uint8_t> &data)
-{
-    _data = data;
-}
-
-bool Packet::isValidPacket() const
-{
-    if(_data[0] != SOH)
+    Packet::Packet(const std::vector<uint8_t> &message)
     {
-        return false;
+        checksum = 0;
+        uint8_t msgLen = message.size() + 3;
+        _data.emplace_back(SOH);    // start of packet
+        _data.emplace_back(msgLen); // message length
+        checksum = SOH + msgLen;
+
+        for (const auto &el : message)
+        {
+            _data.emplace_back(el);
+            checksum += el;
+        }
+
+        checksum = ~(checksum & 0xFF) + 1; // two's complement
+        _data.emplace_back(checksum);
     }
 
-    uint8_t chks = 0;
-    for(const auto & c : _data)
+    Packet::~Packet()
     {
-        chks += c;
-    }
-    if(chks != 0)
-    {
-        return false;
-    }
-    return true;
-}
-
-bool isValidPacket(const std::vector<uint8_t> &data)
-{
-    if(data[0] != SOH)
-    {
-        return false;
     }
 
-    uint8_t chks = 0;
-    for(const auto & c : data)
+    const uint8_t *Packet::data() const
     {
-        chks += c;
-    }
-    if(chks != 0)
-    {
-        return false;
-    }
-    return true;
-}
-
-
-std::string Packet::toString() const
-{
-    std::string str;
-    std::stringstream ss;
-    for(const auto &c : _data)
-    {
-        ss << std::hex << (int)c << ":";
+        return _data.data();
     }
 
-    // remove last character ":" from the string
-    std::string s = ss.str();
-    s.pop_back();
-    
-    return s;
-}
+    size_t Packet::size() const
+    {
+        return _data.size();
+    }
 
+    std::vector<uint8_t> Packet::getData() const
+    {
+        return this->_data;
+    }
+
+    Packet Packet::EmptyPacket()
+    {
+        std::vector<uint8_t> emptyVector{};
+        return Packet(emptyVector);
+    }
+
+    uint8_t Packet::at(const uint8_t pos) const
+    {
+        return _data.at(pos);
+    }
+
+    void Packet::setData(const std::vector<uint8_t> &data)
+    {
+        _data = data;
+    }
+
+    bool Packet::isValidPacket() const
+    {
+        if (_data[0] != SOH)
+        {
+            return false;
+        }
+
+        uint8_t chks = 0;
+        for (const auto &c : _data)
+        {
+            chks += c;
+        }
+        if (chks != 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    bool isValidPacket(const std::vector<uint8_t> &data)
+    {
+        if (data[0] != SOH)
+        {
+            return false;
+        }
+
+        uint8_t chks = 0;
+        for (const auto &c : data)
+        {
+            chks += c;
+        }
+        if (chks != 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    std::string Packet::toString() const
+    {
+        std::string str;
+        std::stringstream ss;
+        for (const auto &c : _data)
+        {
+            ss << std::hex << (int)c << std::dec << ":";
+        }
+
+        // remove last character ":" from the string
+        std::string s = ss.str();
+        s.pop_back();
+
+        return s;
+    }
 
 } // namespace Xerxes
